6f108d80f54be89d257a4ea8270e8aa58de03ea0
Kocsis Gergely <kocsisger@example.com>
2025-12-02 11:08:04 +0100

    persons.html

--- src/main/java/hu/unideb/inf/bootstrap_k10_2/controller/MainController.java
+++ src/main/java/hu/unideb/inf/bootstrap_k10_2/controller/MainController.java
@@ -1,17 +1,25 @@
 package hu.unideb.inf.bootstrap_k10_2.controller;
 
+import hu.unideb.inf.bootstrap_k10_2.model.Person;
+import hu.unideb.inf.bootstrap_k10_2.model.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    PersonRepository personRepository;
+
     @GetMapping("")
     public String showHomepage(){
         return "index";
     }
 
     @GetMapping("/persons")
-    public String showPersons(){
+    public String showPersons(Model model){
+        model.addAttribute("personsList", personRepository.findAll());
         return "persons";
     }
 }
